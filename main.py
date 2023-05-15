a295bb16064b25b6db3c60b1e87830e0cc6efc61
jstzwj <1103870790@qq.com>
2023-05-15 17:19:59 +0800

    update ui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import uuid
 import streamlit as st
 import pickle
 import os
@@ -19,11 +20,10 @@ st.set_page_config(
      }
 )
 
-def build_chat_area(i: int, messages):
-    placeholder = st.empty()
-    with placeholder.form(f"form-{i}", True):
+def build_chat_area(placeholder, form_id: str, messages):
+    with placeholder.form(form_id, True):
         for message in messages:
-            md_dom = st.text_area(label=f"{message['role']}: ",
+            message_dom = st.text_area(label=f"{message['role']}: ",
                                     value=message['content'],
                             height=100,
                             max_chars=2048,
@@ -37,7 +37,13 @@ def build_chat_area(i: int, messages):
             btn_save = st.form_submit_button("保存", use_container_width=True)
         with col4:
             btn_next = st.form_submit_button("下一个对话", use_container_width=True, type="primary")
-    return placeholder, btn_previous, btn_del, btn_save, btn_next
+        
+        st.balloons()
+        print(btn_next)
+        if btn_next:
+            placeholder.empty()
+            st.session_state.index = st.session_state.index + 1
+    return btn_previous, btn_del, btn_save, btn_next
 
 def main():
     with open("dataset.json", "r", encoding="utf-8") as f:
@@ -53,18 +59,10 @@ def main():
 
     md_dom = st.markdown(f"> 对话数据 - {st.session_state.index}")
 
-    index = st.session_state.index
     data = dataset[st.session_state.index]
     messages = data["messages"]
-    container, btn_previous, btn_del, btn_save, btn_next = build_chat_area(index, messages)
-
-    st.balloons()
-
-    if btn_next:
-        container.empty()
-        st.session_state.index += 1
-        container = build_chat_area(index + 1, messages)
-
+    placeholder = st.empty()
+    btn_previous, btn_del, btn_save, btn_next = build_chat_area(placeholder, str(uuid.uuid4()), messages)
 
 if __name__ == "__main__":
     main()
